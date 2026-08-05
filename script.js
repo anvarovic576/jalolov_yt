e7eec9aecb07376ac37a5fdb07d18c0e2ca59d25
anvarovic576 <dalerjalolov668@gmail.com>
2026-08-05 16:13:17 +0500

    Add search functionality to filter news items

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function xabar() {
   alert("Jalolov News saytiga xush kelibsiz!");
-}
+}const search = document.getElementById("search");
+
+search.addEventListener("keyup", function() {
+  let value = search.value.toLowerCase();
+  let news = document.querySelectorAll(".news");
+
+  news.forEach(item => {
+    let text = item.innerText.toLowerCase();
+
+    if (text.includes(value)) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
+});
